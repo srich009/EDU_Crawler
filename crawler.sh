d0d66d4ea1e31e512ea2ef54cffb2e1eb5d19770
srich009 <srich009@ucr.edu>
2018-04-28 13:32:38 -0700

    add part for '.' expansion

--- crawler.sh
+++ crawler.sh
@@ -5,38 +5,46 @@
 # [user@server]$ ./crawler.sh <seed-file> <num-pages> <hops-away> <output-dir>
 #------------------------------------------------------------------------------
 
-# error checking
-if   [ $# -lt 4 ] ; then
-    echo "Error: too few arguments"
-    echo "usage: ./crawler.sh <seed-file> <num-pages> <hops-away> <output-dir>"
-    exit 1
-elif [ $# -ge 5 ] ; then
-    echo "Error: too many arguments"
-    echo "usage: ./crawler.sh <seed-file> <num-pages> <hops-away> <output-dir>"
-    exit 1
-elif ! [ -e exe/web_crawler.jar ] ; then
-    echo "Error: crawler executable not found"
-    exit 1
-elif ! [ -e $1 ] ; then
-    echo "Error: seed-file not found"
-    exit 1
-elif ! [ $2 -ne 0 -o $2 -eq 0 ] ; then
-    echo "Error: number of pages must be integer"
-    exit 1
-elif ! [ $3 -ne 0 -o $3 -eq 0 ] ; then
-    echo "Error: number of hops must be integer"
-    exit 1
-elif ! [ -e $4 ] ; then
-    echo "Error: output directory not found"
-    exit 1
-fi
-#------------------------------------------------------------------------------
-
 # input parameters
 SEED=$1
 PAGES=$2
 HOPS=$3
 OUTPUT=$4
+
+# error checking
+if   [ $# -lt 4 ] ; then
+     echo "Error: too few arguments"
+     echo "usage: ./crawler.sh <seed-file> <num-pages> <hops-away> <output-dir>"
+     exit 1
+elif [ $# -ge 5 ] ; then
+     echo "Error: too many arguments"
+     echo "usage: ./crawler.sh <seed-file> <num-pages> <hops-away> <output-dir>"
+     exit 1
+elif ! [ -e exe/web_crawler.jar ] ; then
+     echo "Error: crawler executable not found"
+     exit 1
+elif ! [ -e $SEED ] ; then
+     echo "Error: seed-file not found"
+     exit 1
+elif ! [ $PAGES -ne 0 -o $PAGES -eq 0 ] ; then
+     echo "Error: number of pages must be integer"
+     exit 1
+elif ! [ $HOPS -ne 0 -o $HOPS -eq 0 ] ; then
+     echo "Error: number of hops must be integer"
+     exit 1
+elif ! [ -e $OUTPUT ] ; then
+     echo "Error: output directory not found"
+     exit 1
+fi
+#------------------------------------------------------------------------------
+
+# do some directory resolving
+# the shell automatically converts: "~"  into  "$HOME"
+# otherwise user should type full path
+if   [ $OUTPUT == "." ] ; then
+     OUTPUT=$PWD
+fi
+#------------------------------------------------------------------------------
 
 date
 echo "starting crawler"
