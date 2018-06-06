6f9e9ec621fcd6821266c82cb81cd5265e3b984e
srich009 <srich009@ucr.edu>
2018-06-06 13:51:07 -0700

    shell script

--- Phase2/go.sh
+++ Phase2/go.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # how to query localhost with string using curl
-# curl http://localhost:8080/search?query=STRING
+# curl http://localhost:8080/search?query=STRING&withPR=BOOL&count=10
+# STRING = query to search for
+# BOOL   = true/false , ude pagerank or not
+# count  = number of pages to show
 
-STEM="http://localhost:8080/search?query="
+FRONT="http://localhost:8080/search?query="
+BACK="&withPR=false&count=10"
 
 while true;
 do
-    echo 
+    echo ""
     echo -n "QUERY>> "
     read QUERY
-    curl $STEM$QUERY
+    curl $FRONT$QUERY$BACK
 done
